ddb7595e0b0e35e07a7b63fb76276d8b87d1dd40
Johnny Awesome <35241451+johnnyawesome@users.noreply.github.com>
2018-10-21 00:44:31 +0200

    Update Favicon Turd.user.js

--- Favicon Turd.user.js
+++ Favicon Turd.user.js
@@ -7,6 +7,8 @@
 // @match        *://*/*
 // @grant        none
 // @icon         https://cdn0.iconfinder.com/data/icons/pixelo/32/skull.png
+// @updateURL    https://github.com/johnnyawesome/FaviconTurd/raw/master/Favicon%20Turd.user.js
+// @downloadURL  https://github.com/johnnyawesome/FaviconTurd/raw/master/Favicon%20Turd.user.js
 // ==/UserScript==
 
 (function() {
